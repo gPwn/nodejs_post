a2acbf9ea143ae8f63dec1ff5ab3320d047cb3e5
hyeju <hyeju3014@gmail.com>
2022-12-01 12:59:44 +0900

    fourth commit

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(express.json());
 // app.use("/api", [commentsRouter,postsRouter]);
 app.use("/", indexsRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hyeju_nodejs_post');
+});
+
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
 });
