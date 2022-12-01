654f6437aeed03b126e6effba006b038bb92bf2e
hyeju <hyeju3014@gmail.com>
2022-12-01 12:16:02 +0900

    third commit

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.use(express.json());
 // app.use("/api", [commentsRouter,postsRouter]);
 app.use("/", indexsRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
 });
